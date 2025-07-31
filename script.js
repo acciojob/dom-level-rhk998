4ac2696253766f2c93f78273ea941036311b103e
rhk998 <130037816+rhk998@users.noreply.github.com>
2025-07-31 23:18:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const targetLevel = document.getElementById("level");
+
+function getDepth(e){
+	let level = 0;
+	while(e){
+		level++;
+		e = e.parentElement
+	}
+	return level;
+}
+const depth = getDepth(targetLevel);
+alert(`The level of the element is: ${depth}`);
+
+const depth = function getDepth(targetLevel);
